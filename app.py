c534dfe7808857f051a47cb82e8f7fe1fb93459b
unknown <a23353723@gmail.com>
2020-05-10 23:19:55 +0800

    commit app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    if(message.find("吉娃娃") >= 0)
-    line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text.find("吉娃娃") >= 0:
+        line_bot_api.reply_message(event.reply_token, text = "老子是地表最會睡吉娃娃")
 
 import os
 if __name__ == "__main__":
