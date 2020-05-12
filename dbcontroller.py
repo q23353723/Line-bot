489431f1a97241da98bbf2c5a213796ade8fcab9
unknown <a23353723@gmail.com>
2020-05-12 21:02:13 +0800

    commit -a

--- dbcontroller.py
+++ dbcontroller.py
@@ -3,7 +3,9 @@ import psycopg2
 
 DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a chihuahua-line-bot').read()[:-1]
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(database="d257c3vevk7b15",user="uvvzexnmjjixeq",password="f7fbeb5e4ec298b1bfa40c0b5020808ad6418ef2ac88b755c102e9a5611346ce",host="ec2-52-202-22-140.compute-1.amazonaws.com",port=5432)
+
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 cursor = conn.cursor()
 
